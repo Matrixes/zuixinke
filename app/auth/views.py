3360c9eafff733894f8d989514f5ee2c8ae24934
kai <kaikty@163.com>
2016-03-07 22:05:33 +0800

    2016_0307_2200

--- app/auth/views.py
+++ app/auth/views.py
@@ -94,6 +94,7 @@ def change_password():
         if current_user.verify_password(form.old_password.data):
             current_user.password = form.password.data
             db.session.add(current_user)
+            db.session.commit()
             flash(u'密码更改成功')
             return redirect(url_for('main.index'))
         else:
