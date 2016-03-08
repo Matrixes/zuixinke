6c3ca85f10de0924c7adca9490b0067855ba2cc6
kai <kaikty@163.com>
2016-03-08 19:22:00 +0800

    add db.session.commit()

--- app/main/views.py
+++ app/main/views.py
@@ -37,6 +37,7 @@ def edit_profile():
         current_user.location = form.location.data
         current_user.about_me = form.about_me.data
         db.session.add(current_user)
+        db.session.commit()
         flash(u'资料已更改')
         return redirect(url_for('main.user', username=current_user.username))
     form.name.data = current_user.name
